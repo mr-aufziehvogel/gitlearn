9037ebda378f1e6af8699558f57e0379d6f08cf1
Kay <kn.vonkiedrowski@gmail.com>
2020-12-10 17:46:56 +0100

    hgh

--- Kontaktliste.py
+++ Kontaktliste.py
@@ -16,28 +16,53 @@ class Contacts:
         self.address = address
 
 def show():
-    pass
+    i = 0
+    for cons in contactlist:
+        print("ID: " + i)
+        print("\nName: " + contactlist[i].name)
+        print("Mail: " + contactlist[i].mail)
+        print("Telefon: " + contactlist[i].phone)
+        print("Adresse: " + contactlist[i].address)
+        i += 1
 
 def create():
     name = input("Name? ")
     mail = input("Mail? ")
     phone = input("Telefonnummer? ")
     address = input("Adresse? ")
-    contactlist.append(Contacts(name,mail,phone,address))
     clear()
+    contactlist.append(Contacts(name,mail,phone,address))
     print("Name: " + contactlist[-1].name)
     print("Mail: " + contactlist[-1].mail)
     print("Telefon: " + contactlist[-1].phone)
-    print("Adresse: " + contactlist[-1].number)
+    print("Adresse: " + contactlist[-1].address + "\n")
 
 def edit():
-    pass
+    id = input("Welchen Kontakt bearbeiten? ")
+    print("\nName: " + contactlist[-1].name)
+    print("Mail: " + contactlist[-1].mail)
+    print("Telefon: " + contactlist[-1].phone)
+    print("Adresse: " + contactlist[-1].address + "\n")
+    while True:
+        which_edit = input("1. Name\n2. Mail\n3. Telefonnummer\n4.Adresse\n")
+        if which_edit == "1"
+            input = ("Neuer Name? ")
+            contactlist[id].name = input
+        elif which_edit == "2"
+            contactlist[id].mail = input
+        elif which_edit == "3"
+            input = ("Neue Telefonnummer? ")
+            contactlist[id].number = input
+        elif which_edit == "4"
+            input = ("Neue Adresse? ")
+            contactlist[id].address = input
+
 
 def delete():
     pass
 
 while True:
-    print("1. Anzeigen")
+    print("\n1. Anzeigen")
     print("2. Erstellen")
     print("3. Bearbeiten")
     print("4. Löschen")
