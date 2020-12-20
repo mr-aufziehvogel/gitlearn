b1166ab9f64e126e0926c1b753e94adb1ed31a6b
Kay <kn.vonkiedrowski@gmail.com>
2020-12-20 18:21:02 +0100

    fwa

---
+++ primzahlen.py
@@ -0,0 +1,21 @@
+limit = input("which range?")
+for i in range(int(limit)+1):
+    for j in range(2,int(limit)+1):
+        if i == j:
+            print(i)
+            continue
+        elif i % j == 0:
+            break
+        elif i % j != 0:
+            pass
+
+# number = input("welche zahl?")
+# number = int(number)
+# for i in range(0,number):
+#     for teiler in range(2, i):
+#         if i == number:
+#             print(number)
+#         elif number % i != 0:
+#             continue
+#         else:
+#             break
